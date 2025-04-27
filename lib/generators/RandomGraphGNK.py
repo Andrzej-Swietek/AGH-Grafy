2d4b5b6218681d16d5935b705edc9ea8dab9c5df
Bartosz Fryska <136595506+BartoszFryska@users.noreply.github.com>
2025-04-27 15:38:56 +0200

    Simplifying generation procedure for weighted
    Simplifying generation procedure for weighted connected graph

--- lib/generators/RandomGraphGNK.py
+++ lib/generators/RandomGraphGNK.py
@@ -6,7 +6,6 @@ from lib.core.adjacency_matrix_graph import AdjacencyMatrixGraph
 from lib.generators.RandomGraphGeneator import RandomGraphGenerator
 from lib.generators.RandomTree import RandomTree
 
-
 class RandomGraphGNK(RandomGraphGenerator):
     def __init__(self, num_vertices: int, num_edges: int):
         self.num_vertices = num_vertices
@@ -50,16 +49,23 @@ class RandomGraphGNK(RandomGraphGenerator):
     
     def generate_connected_weighted(self) -> WeightedGraph:
         if self.num_edges < self.num_vertices - 1:
-            raise ValueError("Too few edges to generate connected graph.")
-        
-        treeGraph = RandomTree(self.num_vertices).generate()
-        edges = set(treeGraph.get_edges())
-        
+            raise ValueError("Too few edges to generate a connected graph.")
+        if self.num_edges > self.num_vertices * (self.num_vertices - 1) // 2:
+            raise ValueError("Too many edges for a simple undirected graph.")
+
         graph = WeightedGraph(self.num_vertices)
-        while len(edges) < self.num_edges:
+        
+        tree = RandomTree(self.num_vertices).generate()
+        edge_set = set()
+        for u, v in tree.get_edges():
+            graph.add_edge(u, v, 0)
+            edge_set.add((min(u, v), max(u, v)))
+
+        while len(edge_set) < self.num_edges:
             u, v = sample(range(self.num_vertices), 2)
-            if (u, v) not in edges and (v, u) not in edges:
+            edge = (min(u, v), max(u, v))
+            if edge not in edge_set:
                 graph.add_edge(u, v, 0)
-                edges.add((u, v))
-                
+                edge_set.add(edge)
+
         return graph
